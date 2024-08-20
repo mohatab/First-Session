f314251198b3887b72959cfd0e04ccef2d0d7708
mohatab <93093669+mohatab@users.noreply.github.com>
2024-08-20 16:20:05 +0300

    Add files via upload

---
+++ first-session.js
@@ -0,0 +1,51 @@
+//js data types >> number , string , object , array , undefined , null , boolean , bigint , map , set , promise .
+
+//declaring variable
+var x;
+let y;
+const z = 20;
+
+//let
+let x = 30;
+function hello() {
+  x = 10;
+}
+console.log(x);
+
+//var
+var x = 30;
+function hello() {
+  x = 10;
+  console.log(x);
+}
+hello();
+console.log(x);
+
+//let, const >> block scope
+//var >> hoisting
+
+//example for data types :
+let A = 10;
+let B = "mohamed";
+let user = { name: "ahmed", age: 21, birth: "17-5-2003" };
+let students = (10, 20, 30.4);
+
+//now the type of the variable
+typeof x;
+
+//set
+const letters = new Set([a, b, c, a]);
+
+//array
+let student = new Array(10, 2, 5, 6);
+
+//concatenation
+x = x = y;
+
+// single line comment
+
+/* 
+multiple
+line
+comment
+*/
